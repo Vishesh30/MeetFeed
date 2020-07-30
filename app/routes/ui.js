fc22e9d31f95d3004b5126aebd6f58d8686db85e
Verma <varun.verma@sap.com>
2020-07-30 14:23:01 +0530

    default sorting added

--- app/routes/ui.js
+++ app/routes/ui.js
@@ -5,7 +5,10 @@ module.exports = function (router) {
 
   router.get("", function (req, res) {
     Post.find((err, postList) => {
-      console.log(postList);
+      //console.log(postList);
+      postList.sort((f,s) => {
+        return (s.upVotes - s.downVotes) - (f.upVotes - f.downVotes);
+      });
       res.render("index", {postList: postList});
     });
   });
