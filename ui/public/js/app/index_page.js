dca99cf171c03a21b1aae210a919e3d62058be11
Verma <varun.verma@sap.com>
2020-07-30 11:14:31 +0530

    Posting question and voting works

--- ui/public/js/app/index_page.js
+++ ui/public/js/app/index_page.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+
+    $('.upVote-Button').on('click', function () {
+        const postId = $(this).attr('post-id');
+        upVote(postId);
+    });
+
+    $('.downVote-Button').on('click', function () {
+        const postId = $(this).attr('post-id');
+        downVote(postId);
+    });
+
 });
 
 window.addQuestion = function(){
@@ -15,14 +26,48 @@ window.addQuestion = function(){
     }
     console.log(post_data);
 
+    $.ajaxSetup({
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
     $.post("api/post",
         JSON.stringify(post_data))
         .done(function () {
-            alert("question added... reload page");
+            location.reload(); //alert("question added... reload page");
         })
         .fail(function (jqXHR, textStatus, errorThrown) {
             //console.log({ status: textStatus, error_message: jqXHR.responseText });
             const msgData = JSON.parse(jqXHR.responseText);
             alert("Error in adding question: " + msgData);
         });
+}
+
+function upVote(postId){
+
+    console.log(postId);
+    $.post("api/post/" + postId + "/upVote")
+        .done(function () {
+            location.reload(); //alert("upvote done... reload page");
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            //console.log({ status: textStatus, error_message: jqXHR.responseText });
+            const msgData = JSON.parse(jqXHR.responseText);
+            alert("Error in adding vote: " + msgData);
+        });
+}
+
+function downVote(postId){
+
+    console.log(postId);
+    $.post("api/post/" + postId + "/downVote")
+        .done(function () {
+            location.reload(); //alert("downVote done... reload page");
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            //console.log({ status: textStatus, error_message: jqXHR.responseText });
+            const msgData = JSON.parse(jqXHR.responseText);
+            alert("Error in adding vote: " + msgData);
+        });
+
 }
