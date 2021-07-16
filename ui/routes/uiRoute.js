19add70a257bf09daf0ed8ff4a2468c71f8df44f
Vishesh Agrawal <vishesh.agrawal@sap.com>
2021-07-16 16:40:19 +0530

    Handling of backend call for Post

--- ui/routes/uiRoute.js
+++ ui/routes/uiRoute.js
@@ -1,44 +1,40 @@
 var Post = require("../../app/models/post");
 const axios = require("axios");
 
-
 module.exports = function (router) {
-
-  router.get("", async function (req, res) { 
-    let url = 'http://'+ req.headers.host + '/api/event';
-    xdata = await axios.get(url);
-    res.render("home",{eventList: xdata.data});
+  router.get("", async function (req, res) {
+    let url = "http://" + req.headers.host + "/api/event";
+    respData = await axios.get(url);
+    res.render("home", { eventList: respData.data });
   });
 
   router.get("/event/:eventId/showposts", async function (req, res) {
-    
     //getPosts
     let sortOrder = "votes";
-    if(req.cookies.sortOrder){
+    if (req.cookies.sortOrder) {
       sortOrder = req.cookies.sortOrder;
     }
 
-    console.log(Date() + "---" + req.ip + "---" + req.connection.remoteAddress + "---" + sortOrder);
-
     let eventId = req.params.eventId;
 
-    Post.find({eventId: eventId},(err, postList) => {
-      
-      if(sortOrder === "votes"){
-        postList.sort((f,s) => {
-          return (s.upVotes - s.downVotes) - (f.upVotes - f.downVotes);
-        });
-      }
-      else{
-        // TODO implement sorting by time
-        postList.sort((f,s) => {
-          return (new Date(s.commentTime)).getTime() - (new Date(f.commentTime)).getTime();
-        });
-      }
+    let url = "http://" + req.headers.host + "/api/event/" + eventId + "/post";
+    respData = await axios.get(url);
+    let postList = respData.data;
 
-      res.render("index", {postList: postList, event: eventId});
+    if (sortOrder === "votes") {
+      postList.sort((f, s) => {
+        return s.upVotes - s.downVotes - (f.upVotes - f.downVotes);
+      });
+    } else {
+      // TODO implement sorting by time
+      postList.sort((f, s) => {
+        return (
+          new Date(s.commentTime).getTime() - new Date(f.commentTime).getTime()
+        );
+      });
+    }
 
-    });
+    res.render("index", { postList: postList, event: eventId });
   });
 
   router.get("/privacy", function (req, res) {
